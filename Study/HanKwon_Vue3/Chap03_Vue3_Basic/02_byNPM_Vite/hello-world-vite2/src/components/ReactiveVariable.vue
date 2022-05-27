bb99c6772c660fb35a09a36365ab745389a75ed4
hororoc <exploit017@gmail.com>
2022-05-27 18:41:23 +0900

    update

---
+++ Study/HanKwon_Vue3/Chap03_Vue3_Basic/02_byNPM_Vite/hello-world-vite2/src/components/ReactiveVariable.vue
@@ -0,0 +1,33 @@
+<template>
+  <p>줄임말과 원래말을 입력하세요.</p>
+  <input type="text" :value="abbr"/>
+  <input type="text" v-model="normal">
+  <hr/>
+  <abbr :title="normal">{{ abbr }}</abbr>
+</template>
+
+<script>
+import { ref } from 'vue'
+
+export default {
+  // Composition API
+  setup() {
+    const abbr = ref('DOPT')
+    const normal = ref('Dong Project Team')
+
+    return {
+      abbr,
+      normal,
+    }
+  }
+
+}
+
+const count = ref(0)
+</script>
+
+<style scoped>
+a {
+  color: #42b983;
+}
+</style>

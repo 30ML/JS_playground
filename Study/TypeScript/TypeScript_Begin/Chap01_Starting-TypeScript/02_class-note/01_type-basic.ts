f5e9aa0f6912dacd5a861458964fbf79a7d8825b
Min Jae Kim <exploit017@gmail.com>
2022-05-30 23:11:30 +0900

    update

---
+++ Study/TypeScript/TypeScript_Begin/Chap01_Starting-TypeScript/02_class-note/01_type-basic.ts
@@ -0,0 +1,13 @@
+// JS 문자열 선언
+var jsStr = 'hello'
+
+// TS 문자열 선언
+const tsStr: string = 'hello'
+
+// TS 숫자
+const tsNum: number = 10
+
+// TS 배열
+const tsArr: Array<number> = [1,2,3];
+const heros: Array<string> = ['Capt', 'Thor', 'Hulk']
+const items: number[] = [1,2,3]

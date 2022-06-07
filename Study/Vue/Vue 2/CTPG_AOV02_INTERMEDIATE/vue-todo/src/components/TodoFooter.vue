ac6ca9f64f9d02a5814b5f36a1bafe2826f77a9d
hororoc <exploit017@gmail.com>
2022-06-07 11:00:38 +0900

    footer done

--- Study/Vue/Vue 2/CTPG_AOV02_INTERMEDIATE/vue-todo/src/components/TodoFooter.vue
+++ Study/Vue/Vue 2/CTPG_AOV02_INTERMEDIATE/vue-todo/src/components/TodoFooter.vue
@@ -1,15 +1,33 @@
 <template>
   <div>
-    FOOTER
+    <div class="clearAllContainer">
+      <span class="clearAllBtn" v-on:click="clearTodo">Clear All</span>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  
+  methods: {
+    clearTodo: function(){
+      localStorage.clear();
+    },
+  },
 }
 </script>
 
 <style scoped>
-  
+.clearAllContainer {
+  width: 8.5rem;
+  height: 58px;
+  line-height: 58px;
+  background-color: white;
+  border-radius: 5px;
+  margin: 0 auto;
+}
+.clearAllBtn {
+  color: #E20383;
+  /* 추가 */
+  display: block;
+}
 </style>
